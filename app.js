6121764cce0de8de38189dab9604552fa3d12cd0
awsmcod <asimcoskun31@gmail.com>
2022-08-09 23:09:34 +0300

    datatypes

---
+++ app.js
@@ -0,0 +1,74 @@
+// console.log('App js Running')
+
+
+// var a = 5;
+// console.log(a);
+
+// {
+//     a = 3;
+//     console.log(a);
+// }
+
+// var a = 5;
+// console.log(a);
+
+/* console.log(typeof 0)
+console.log(typeof 3.14)
+console.log(typeof 'Hello')
+console.log(typeof (8 > 10))
+console.log(8 > 10)
+console.log(typeof '') */
+
+// let b = +prompt('Bir rakam giriniz')
+
+// console.log(b, typeof b)
+
+// console.log( typeof 'number')
+
+// let age =3;
+
+// console.log(age)
+
+// console.log(myAge)
+
+// var myAge = 40;
+// var myAge1 = 40;
+// var myAge2 = 40;
+// var myAge3 = 40;
+// var myAge4 = 40;
+
+// console.log('Hello')
+
+// let a = null
+
+// console.log( typeof a)
+
+
+// let x;
+
+// do {
+//     x = prompt('enter a Number : ');
+// }while (isNaN(x));
+
+// console.log(x, x*x) 
+
+// console.log('12' === 12)
+
+// console.log('12' * '12')
+
+
+// let x = 123456789001234
+// let y = 123456789001234123
+
+// let z = 0.12345678901234567
+// let zz = 0.123456789012345675678
+
+// console.log({x})
+// console.log({y},typeof y)
+
+// console.log(z)
+// console.log(zz)
+
+let g = 0.1+0.2
+console.log(g)
+console.log(+g.toFixed(2))
